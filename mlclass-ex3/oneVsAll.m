08e5ff7caa81be1e3bd61ca287e201b8e74c162e
Yueleng <wangyueleng@gmail.com>
2014-11-02 21:26:48 +0100

    Update oneVsAll.m

--- mlclass-ex3/oneVsAll.m
+++ mlclass-ex3/oneVsAll.m
@@ -48,7 +48,7 @@ X = [ones(m, 1) X];
 %         fmincg (@(t)(lrCostFunction(t, X, (y == c), lambda)), ...
 %                 initial_theta, options);
 %
-
+initial_theta = zeros(n+1, 1);
 options = optimset('GradObj', 'on', 'MaxIter', 50);
 for c = 1:num_labels
     initial_theta = all_theta(c, :)';
